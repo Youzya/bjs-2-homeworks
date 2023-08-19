86e3b85b068ec65c45425acf43d4f2fe9d3da003
Youzya <141143272+Youzya@users.noreply.github.com>
2023-08-19 13:44:12 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,41 +1,13 @@
-"use strict"
+"use strict";
 
-// 2x^2 + 4x - 3=0
-// ax^2 + bx + c = 0
-function getResult(a, b, c) {
-	// b2 - 4ac
-	let d = b ** 2 + 4 * a * c;
-	if (d > 0) {
-		let x1 = (Math.sqrt(d) - b) / (2 * a);
-		let x2 = (Math.sqrt(d) + b) / (-2 * a);
-		console.log('x1 = ' + x1 + '   x2 = ' + x2);
-	} else if (d == 0) {
-		x1 = b / (-2 * a);
-		console.log('x = ' + x1);
-	} else {
-		console.log('Решений нет, так как дискриминант меньше нуля.');
-	}
-}
-let a = 3;
-let b = 0;
-let c = 48;
-getResult(a, b, c);
-
-
-function getAverageMark(marks) {
-	if (marks.length > 5) {
-		console.log('количество оценок больше пяти');
-		do {
-			marks.pop()
-		} while (marks.length > 5);
-	}
-
-	let sum = 0;
-	for (let i = 0; i < marks.length; i++) {
-		sum += marks[i];
-	}
-
-	let averageMark = sum / marks.length;
-
-	return averageMark
-}
+function solveEquation(a, b, c) {
+  let arr = [];
+  let d = Math.pow(b, 2) - (4 * a * c);
+  if (d < 0) {
+    //return arr;
+  } else if (d == 0) {
+    arr[0] = (-b / (2 * a));
+  } else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  }
